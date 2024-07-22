7ffb662bd2dcc1a270e5066b4435122765288a2a
Anthony Canales <anthony.code8@gmail.com>
2024-07-22 14:53:11 -0400

    feat: :fire: First commit with version 1.0 to personal portfolio based on template in astro

---
+++ src/data/experience.ts
@@ -0,0 +1,67 @@
+import type { Props as ExperienceProp } from "../components/experience/props";
+
+export const EXPERIENCE: ExperienceProp[] = [
+  {
+    dates: "Abr 2021 — Abr 2024",
+    title: "Desarrollador Front-End",
+    company: "Want Agencia de Diseño",
+    companyUrl: "https://want.cl/",
+    description:
+      "Durante mi trayectoria como desarrollador Front-end, tuve el privilegio de liderar la creación de sitios web desde cero en WordPress. Utilicé mis conocimientos para transformar los diseños proporcionados por el equipo de diseñadores en sitios web funcionales y altamente customizables además de visualmente atractivos y con una buena performance de accesibilidad y velocidad.",
+    technologies: [
+      "Wordpress",
+      "SASS/PostCSS",
+      "HTML5",
+      "PHP",
+      "CSS",
+      "Composer",
+      "GIT",
+      "Tailwind",
+      "Javascript",
+      "Themosis",
+      "SCRUM"
+    ],
+    logo: "/logos/want-logo.jpeg",
+  },
+  {
+    dates: "Mar 2018 — Feb 2021",
+    title: "Desarrollador web",
+    company: "Zenbyte",
+    companyUrl: "https://zenbyte.cl/",
+    description:
+      "Durante mi trayectoria, he desarrollado sitios web altamente customizables basados en WordPress. Además Tuve la oportunidad de ser el responsable del front del sitio web de GMO. En este proyecto, lideré el desarrollo de la interfaz de usuario, asegurando que cumpliera con los estándares requeridos de diseño y usabilidad.",
+    technologies: [
+      "Wordpress",
+      "HTML5",
+      "CSS",
+      "Javascript",
+      "GIT",
+      "Magento",
+      "Laravel",
+      "PHP",
+      "SASS/PostCSS",
+      "Bootstrap"
+    ],
+    logo: "/logos/zenbyte-logo.jpeg",
+  },
+  {
+    dates: "May 2016 — May 2018",
+    title: "Desarrollador web",
+    company: "Fixu",
+    companyUrl: "https://fixu.cl/",
+    description:
+      "Durante mi experiencia en el mantenimiento de sitios web, tuve la oportunidad de involucrarme en la toma de requerimientos, proponer soluciones y emitir informes relacionados con analytics, AdWords y Facebook Ads. Trabajé en el desarrollo de sitios web, desde la planificación inicial hasta su implementación y optimización continua.",
+      technologies: [
+        "Wordpress",
+        "HTML5",
+        "CSS",
+        "Javascript",
+        "GIT",
+        "GULP",
+        "Bitbucket",
+        "Bootstrap",
+        "SASS",
+      ],
+    logo: "/logos/fixu-logo.jpeg",
+  },
+];
---
+++ src/data/socials.ts
@@ -0,0 +1,22 @@
+import IconDribbble from "../components/icons/dribbble.astro";
+import IconGithub from "../components/icons/github.astro";
+import IconLinkedin from "../components/icons/linkedin.astro";
+import IconTwitter from "../components/icons/twitter.astro";
+
+export const SOCIALS = [
+  {
+    name: "Github",
+    url: "https://github.com/AnthCa",
+    icon: IconGithub,
+  },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/anthony-canales-ampuero-157928114/",
+    icon: IconLinkedin,
+  },
+  {
+    name: "Twitter",
+    url: "https://x.com/Anthc_a",
+    icon: IconTwitter,
+  }
+];
